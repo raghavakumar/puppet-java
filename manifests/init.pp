0de03c3c04b8ba395a80be98adf0ffe4697a6e07
raghavakumar <raghava.crl@gmail.com>
2014-04-07 16:49:25 +0530

    0# Given proper indentation
    Change-Id: I7ed197f6baea5118a23a7676cefc0543800d931c

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,58 +11,59 @@ class java
   }
 
   file { "${java::params::tmp_directory}/${java::params::java_archive}" :
-    ensure => "present",
-	  source => "puppet:///modules/java/${java::params::java_archive}",
-	  owner => "${java::params::owner}",
-	  mode => 755
-  }
+         ensure => "present",
+	 source => "puppet:///modules/java/${java::params::java_archive}",
+	 owner => "${java::params::owner}",
+	 mode => 755
+       }
 
-  exec { 'extract jdk':
-    cwd => "${java::params::tmp_directory}",
- 	  command => "tar xfv ${java::params::java_archive}",
-	  creates => "${java::params::java_home}",
-	  require => File["${java::params::tmp_directory}/${java::params::java_archive}"],
-  }
+  exec { "extracti_jdk":
+         cwd => "${java::params::tmp_directory}",
+ 	 command => "tar xfv ${java::params::java_archive}",
+	 creates => "${java::params::java_home}",
+         alias => "extract_jdk",
+	 require => File["${java::params::tmp_directory}/${java::params::java_archive}"],
+       }
 
   file { "${java::params::java_base_path}":
-     ensure => directory,
-	   owner => "${java::params::owner}",
-	   require => Exec['extract jdk']
-  }
+         ensure => directory,
+	 owner => "${java::params::owner}",
+	 require => Exec['extract_jdk']
+       }
 
-  exec { 'move jdk':
-     cwd     => "${java::params::tmp_directory}",
-     creates => "${java::params::java_home}",
-     require => File["${java::params::java_base_path}"],
-     command => "mv ${java::params::java_folder} ${java::params::java_base_path}"
-  }
+  exec { "move_jdk":
+         cwd => "${java::params::tmp_directory}",
+         creates => "${java::params::java_home}",
+         require => File["${java::params::java_base_path}"],
+         command => "mv ${java::params::java_folder} ${java::params::java_base_path}"
+       }
 
-  exec {'install java':
-     require   => Exec['move jdk'],
-     logoutput => true,
-     command   => "update-alternatives --install /usr/bin/java java ${java::params::java_home}/bin/java 1"
-  }
+  exec { "install_java":
+         require   => Exec['move_jdk'],
+         logoutput => true,
+         command   => "update-alternatives --install /usr/bin/java java ${java::params::java_home}/bin/java 1"
+       }
 
-  exec {'set java':
-      require   => Exec['install java'],
-      logoutput => true,
-      command   => "update-alternatives --set java ${java::params::java_home}/bin/java"
-  }
+  exec { "set_java":
+         require   => Exec['install_java'],
+         logoutput => true,
+         command   => "update-alternatives --set java ${java::params::java_home}/bin/java"
+       }
 
-  exec {'install javac':
-      require   => Exec['move jdk'],
-      logoutput => true,
-      command   => "update-alternatives --install /usr/bin/javac javac ${java::params::java_home}/bin/javac 1"
-  }
+  exec { "install_javac":
+         require   => Exec['move_jdk'],
+         logoutput => true,
+         command   => "update-alternatives --install /usr/bin/javac javac ${java::params::java_home}/bin/javac 1"
+       }
 
-  exec {'set javac':
-      require   => Exec['install javac'],
-      logoutput => true,
-      command   => "update-alternatives --set javac ${java::params::java_home}/bin/javac"
-  }
+  exec { "set_javac":
+         require   => Exec['install_javac'],
+         logoutput => true,
+         command   => "update-alternatives --set javac ${java::params::java_home}/bin/javac"
+       }
 
   file { "/etc/profile.d/java.sh":
-      content => "export JAVA_HOME=${java::params::java_home}
+    content => "export JAVA_HOME=${java::params::java_home}
 export PATH=\$PATH:\$JAVA_HOME/bin"
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,29 +2,27 @@
 
 class java::params {
 
+   $java_archive = $::hostname ? {		
+      default => "jdk-7u51-linux-x64.tar.gz",
+   }
 
-	$java_archive = $::hostname ? {		
-		default	=> "jdk-7u51-linux-x64.tar.gz",
-	}
+   $java_base_path = $::hostname ? {
+      default => "/usr/lib/jvm",
+   }
 
-  $java_base_path = $::hostname ? {
-    default => "/usr/lib/jvm",
-  }
+   $java_home = $::hostname ? {
+      default => "/usr/lib/jvm/jdk1.7.0_51",
+   }
 
-	$java_home = $::hostname ? {
-	  default	=> "/usr/lib/jvm/jdk1.7.0_51",
-	}
+   $java_folder = $::hostname ? {
+      default => "jdk1.7.0_51",
+   }
 
-
-	$java_folder = $::hostname ? {
-		default	=> "jdk1.7.0_51",
-	}
-
-  $tmp_directory = $::hostname ? {
-    default => "/tmp",
-  }
+   $tmp_directory = $::hostname ? {
+      default => "/tmp",
+   }
   
-  $owner = $::hostname ? {
-    default => "root",
-  }
+   $owner = $::hostname ? {
+      default => "root",
+   }
 }
